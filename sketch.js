b31a62ddbd56612bdd2aa6d4fbca70cf1968f025
maverick13m <71173739+maverick13m@users.noreply.github.com>
2020-11-11 19:59:54 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,64 @@
+var ball;
+var database
+var position
+
+function setup(){
+    createCanvas(500,500);
+    database = firebase.database()
+    var child = database.ref('ball/position')
+    child.on("value",readposition,showERROR)
+
+
+
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+    database.ref("ball/position").set({
+    x:ball.x + x,
+    y:ball.y + y
+
+
+
+    })
+}
+
+function readposition(data){
+    position=data.val()
+    ball.x = position.x
+    ball.y = position.y
+}
+
+function showERROR(){
+    console.log("ERROOR")
+
+
+
+}
+
+
+
+
+
+
+
+
+
